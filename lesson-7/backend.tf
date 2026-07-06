73a9371c1535a34c0611becd15504c58da37292f
Alex Sander <alex.darienko@gmail.com>
2026-07-06 13:45:58 +0300

    Fix: uncomment backend.tf, configure EKS access, add env vars and k8s probes

--- lesson-7/backend.tf
+++ lesson-7/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "alex-devops-tfstate-lesson7"
-#     key            = "lesson-7/terraform.tfstate"
-#     region         = "us-west-2"
-#     dynamodb_table = "terraform-locks-l7"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "alex-devops-tfstate-lesson7"
+    key            = "lesson-7/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-locks-l7"
+    encrypt        = true
+  }
+}
--- lesson-7/modules/eks/eks.tf
+++ lesson-7/modules/eks/eks.tf
@@ -1,20 +1,28 @@
 
-# IAM Role
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
   assume_role_policy = jsonencode({ Version = "2012-10-17", Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "eks.amazonaws.com" } }] })
 }
 resource "aws_iam_role_policy_attachment" "cluster_policy" { policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"; role = aws_iam_role.cluster.name }
 
-# ClusterK8s
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
-  vpc_config { subnet_ids = var.subnet_ids }
+
+  vpc_config {
+    subnet_ids              = var.subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   depends_on = [aws_iam_role_policy_attachment.cluster_policy]
 }
 
-# IAM Role
 resource "aws_iam_role" "node" {
   name = "${var.cluster_name}-node-role"
   assume_role_policy = jsonencode({ Version = "2012-10-17", Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "ec2.amazonaws.com" } }] })
@@ -23,13 +31,12 @@ resource "aws_iam_role_policy_attachment" "node_policy" { policy_arn = "arn:aws:
 resource "aws_iam_role_policy_attachment" "cni_policy" { policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"; role = aws_iam_role.node.name }
 resource "aws_iam_role_policy_attachment" "ecr_policy" { policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"; role = aws_iam_role.node.name }
 
-# Node Group
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.subnet_ids
-  instance_types  = ["t3.medium"] # Мінімальний рекомендований для EKS
+  instance_types  = ["t3.medium"]
   scaling_config {
     desired_size = 2
     max_size     = 6
